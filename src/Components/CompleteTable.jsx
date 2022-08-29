d65e1ba0ca4473a799c9d8342dfbdd8820757d93
rahkned1990 <rahk1990@gmail.com>
2022-08-29 11:01:09 -0400

    Added table to both table components

--- src/Components/CompleteTable.jsx
+++ src/Components/CompleteTable.jsx
@@ -1,6 +1,23 @@
 const CompleteTable = (props) => {
     return ( 
         <>
+        <h2>Completed Tasks</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>Task ID</th>
+                    <th>Task</th>
+                    <th>Task Options</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Clean Cat</td>
+                    <td>1</td>
+                </tr>
+            </tbody>
+        </table>
         </>
      );
 }
--- src/Components/IncompleteTable.jsx
+++ src/Components/IncompleteTable.jsx
@@ -1,7 +1,24 @@
 const IncompleteTable = (props) => {
     return ( 
-    <>
-    </>
+        <>
+        <h2>Incompleted Tasks</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>Task ID</th>
+                    <th>Task</th>
+                    <th>Task Options</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Clean Cat</td>
+                    <td>1</td>
+                </tr>
+            </tbody>
+        </table>
+        </>
     );
 }
  
