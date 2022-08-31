1e4f0e3daaa1e6d7e3882f42ace24fce5756f770
rahkned1990 <rahk1990@gmail.com>
2022-08-31 15:28:01 -0400

    Add editContent component to table

--- src/Components/IncompleteTable.jsx
+++ src/Components/IncompleteTable.jsx
@@ -1,30 +1,33 @@
-import React from 'react';
-import ReadOnly from './EditorReadOnly/ReadOnly';
-// import EditContent from './ButtonComponents/EditContent';
+import React, { Fragment } from "react";
+import EditContent from "./EditorReadOnly/EditContent";
+import ReadOnly from "./EditorReadOnly/ReadOnly";
 
 
 const IncompleteTable = (props) => {
+  return (
+    <>
+      <h2>Incompleted Tasks</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Task ID</th>
+            <th>The Task At Hand</th>
+            <th>Options</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.parentTasks.map((parentTask, index) => {
+            return (
+              <Fragment>
+                <EditContent />
+                <ReadOnly parentTask={parentTask} index={index} />
+              </Fragment>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+};
 
-    
-    return ( 
-        <>
-        <h2>Incompleted Tasks</h2>
-        <table>
-            <thead>
-                <tr>
-                    <th>Task ID</th>
-                    <th>The Task At Hand</th>
-                    <th>Options</th>
-                </tr>
-            </thead>
-            <tbody>
-            {props.parentTasks.map((parentTask, index) => {
-                 return(<ReadOnly parentTask={parentTask} index={index}/>);
-                })}
-            </tbody>
-        </table>
-        </>
-    );
-}
- 
 export default IncompleteTable;
